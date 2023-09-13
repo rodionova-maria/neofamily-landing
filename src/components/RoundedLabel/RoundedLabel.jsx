e92d73b4fe2c89e715cdfed5a9e9cf6481c32d24
rodionova-maria <mar_r@mail.ru>
2023-09-13 23:22:33 +0300

    svg loaded as modules and spring animation is added

--- src/components/RoundedLabel/RoundedLabel.jsx
+++ src/components/RoundedLabel/RoundedLabel.jsx
@@ -1,7 +1,33 @@
+import { useEffect, useState } from 'react'
 import s from './RoundedLabel.module.scss'
+import { animated, useSpring } from 'react-spring'
 
 const RoundedLabel = ({ children }) => {
-  return <span className={s.text}>{children}</span>
+  const [isRotating, setIsRotating] = useState(true)
+
+  const rotateAnimation = useSpring({
+    transform: isRotating
+      ? 'rotate(13deg) translateX(-100%) translateY(100%)'
+      : 'rotate(12deg) translateX(-100%) translateY(100%)',
+    config: { tension: 170, friction: 12 },
+  })
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIsRotating((prev) => !prev)
+    }, 300)
+
+    return () => clearInterval(interval)
+  }, [])
+
+  return (
+    <animated.span
+      className={s.text}
+      style={{ ...rotateAnimation, transformOrigin: 'center center' }}
+    >
+      {children}
+    </animated.span>
+  )
 }
 
 export default RoundedLabel
